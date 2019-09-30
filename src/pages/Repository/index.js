312c825030d184615100d1df8b93e0e2edd219e4
Enio Costa <enio.m.costa@gmail.com>
2019-09-30 20:19:57 -0300

    Done repository issues list implementation

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -6,7 +6,7 @@ import Container from '../../components/Container';
 
 import api from '../../services/api';
 
-import { Loading, Owner } from './styles';
+import { Loading, Owner, IssueList } from './styles';
 
 export default class Repository extends Component {
   constructor(props) {
@@ -43,6 +43,8 @@ export default class Repository extends Component {
   render() {
     const { repository, issues, loading } = this.state;
 
+    console.log(issues);
+
     if (loading) {
       return (
         <Loading>
@@ -60,6 +62,23 @@ export default class Repository extends Component {
           <h1>{repository.name}</h1>
           <p>{repository.description}</p>
         </Owner>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
       </Container>
     );
   }
